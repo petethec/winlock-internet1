80896c008b226d4d798614d641e6cde8d8799ace
petethec <pcrosby@savvynets.com>
2016-08-28 22:13:29 -0700

    First Commit
    With some minor config

---
+++ survey_data.js
@@ -0,0 +1,15 @@
+// Hard coded survey questions
+module.exports = [
+    {
+        text: 'Do you have Internet service?',
+        type: 'boolean'
+    },
+    {
+        text: 'Are you happy with your service?',
+        type: 'boolean'
+    },
+    {
+        text: 'What is your street address?',
+        type: 'text'
+    }
+];
